bc038a982845eea2797a810056dde9eea9c97c9b
Kunal Singh <kunalsingh.k@flipkart.com>
2017-01-14 02:25:53 +0530

    local commit

---
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -0,0 +1,28 @@
+package com.example;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+/**
+ * Created by kunalsingh.k on 07/01/17.
+ */
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(String [] args) throws Exception{
+        System.out.println("Hello World "+args.toString());
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration helloWorldConfiguration, Environment environment) throws Exception {
+        final HelloWorldResource resource = new HelloWorldResource(helloWorldConfiguration.getTemplate(), helloWorldConfiguration.getDefaultName());
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck();
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+}
---
+++ src/main/java/com/example/TemplateHealthCheck.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.codahale.metrics.health.HealthCheck;
+
+/**
+ * Created by kunalsingh.k on 07/01/17.
+ */
+public class TemplateHealthCheck extends HealthCheck {
+
+    @Override
+    protected Result check() throws Exception {
+        return null;
+    }
+}
