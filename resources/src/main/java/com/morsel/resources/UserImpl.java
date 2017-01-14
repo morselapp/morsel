3029ce061fc8614e37b6621ce8746cc326a58c94
Ajeet Singh <ajeet2808@gmail.com>
2017-01-14 12:45:29 +0530

    changed the pom

--- resources/src/main/java/com/morsel/resources/UserImpl.java
+++ resources/src/main/java/com/morsel/resources/UserImpl.java
@@ -1,9 +1,23 @@
 package com.morsel.resources;
 
-import com.morsel.*;
+import com.morsel.models.*;
+
+import java.util.List;
 
 /**
  * Created by ajeet on 14/1/17.
  */
-public class UserImpl{
+public class UserImpl implements User{
+
+    public List<Order> getOrders() {
+        return null;
+    }
+
+    public List<Order> getOrder() {
+        return null;
+    }
+
+    public Address getAddress() {
+        return null;
+    }
 }
