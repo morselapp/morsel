13e06d42ef3ff1ccec36189620998a509ccaa0e8
Ajeet Singh <ajeet2808@gmail.com>
2017-01-14 20:16:14 +0530

    Added config

---
+++ service/src/main/java/com.morsel.service/config/HelloWorldConfiguration.java
@@ -0,0 +1,48 @@
+package com.morsel.service.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * Created by kunalsingh.k on 07/01/17.
+ */
+public class HelloWorldConfiguration extends io.dropwizard.Configuration{
+
+    @NotEmpty
+    private String template ="OK";
+
+    @NotEmpty
+    private String defaultName ="OK";
+
+    @JsonProperty
+    public void setTemplate(String template){
+        this.template = template;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName){
+        this.defaultName = defaultName;
+    }
+
+    @JsonProperty
+    public String getTemplate(){
+        return template;
+    }
+
+    @JsonProperty
+    public String getDefaultName(){
+        return defaultName;
+    }
+
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
+
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.dataSourceFactory = dataSourceFactory;
+    }
+
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
+}
--- service/src/main/java/com.morsel.service/service/HelloWorldApplication.java
+++ service/src/main/java/com.morsel.service/service/HelloWorldApplication.java
@@ -1,16 +1,21 @@
 package com.morsel.service.service;
-import com.morsel.service.HelloWorldConfiguration;
+
+import com.morsel.models.*;
+import com.morsel.service.config.HelloWorldConfiguration;
 import com.morsel.service.HelloWorldResource;
 import com.morsel.service.TemplateHealthCheck;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.hibernate.Hibernate;
 
 /**
  * Created by kunalsingh.k on 07/01/17.
  */
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
-
+    private HibernateBundle<HelloWorldConfiguration> hibernateBundle ;
     public static void main(String [] args) throws Exception{
         System.out.println("Hello World "+args.toString());
         new HelloWorldApplication().run(args);
@@ -18,7 +23,16 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 
     @Override
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        hibernateBundle = new HibernateBundle<HelloWorldConfiguration>(User.class,
+                MenuItem.class,
+                Order.class,
+                OrderItem.class) {
 
+            public DataSourceFactory getDataSourceFactory(HelloWorldConfiguration helloWorldConfiguration) {
+                return helloWorldConfiguration.getDataSourceFactory();
+            }
+        };
+        bootstrap.addBundle(hibernateBundle);
     }
 
     @Override
